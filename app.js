03a378d18a75944293955d730bf83589198b65bd
hanyangpaca <dlwogjs0125@hanyang.ac.kr>
2022-04-12 22:51:48 +0900

    main

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-alert('hi')
+
+let a = 5; 
+let b = 2;
+let myName= "paca";
+var c = 5;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log('hello'+ myName);
+
+//2.3
+// let = variable constant = invariable
+// if you let a = 2 , let a = 3 -----> a=3 
+// but if you const a = 2 , const a = 3 ------> error
+// 이런식으로 코드를 짜놓으면 const는 아! 이 사람이 안 바꾸고 싶어했구나 알수 있다.
+// 이런 의도 파악에 크게 도움이 되는 기술이다. 
+// 옛날에는 var을 썼었다. var만 쓰던 시절이 있었는데 의도 파악이 안됐다.
+// 그래서 var -> const , let 두가지를 사용하게 되었다. var을 사용할수는 있다.
+
