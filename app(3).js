f54e4f130d5fc1b98a25ec43e4f531a2baa2cef6
hanyangpaca <dlwogjs0125@hanyang.ac.kr>
2022-04-15 00:13:43 +0900

    main

--- app(3).js
+++ app(3).js
@@ -67,11 +67,110 @@ player.sayHello("nico");
 // console.log(player.name)의 경우는 player에서 name를 부르는거고 ( 하나의 string이니까 )
 // 반면에 player.sayHello(); 의 경우는 자체로 함수라서 바로 소환이 가능하다. 이게 함수를 만드는 JS method!
 
-// 2.9 Recap 
+// 2.9 Recap ( variable, datatype = array..)
 
 const a = 5; 
 let b = 6; 
 var c = 7; 
 
-// 우리가 variable을 지정하는 방법은 총 3가지였다. 
+// 우리가 variable을 지정하는 방법은 총 3가지였다.
+
+const d = null ; 
+let hello;
+
+// 이 경우는 d 안에 아무것도 넣고 싶지 않다는 의사표현이다.
+// hello 같은 경우는 undefined 이다. 존재는 하는데 아무것도 없다. 추가를 하고 싶을수도 있고.. 뭐.. 
+// 여러가지 의사가 표현될 수 있다. 개발적 관점 
+
+const days = [1, 2, false, true, null, undefined];
+
+// days[0] 은 파이썬 인덱스처럼 1이다. 
+// days[0]= pizza 하면 1이 pizza로 바뀐다. 
+// days.push("hamburger") 하면 제일 끝에 hamburger이 추가된다.
+
+// 2.10 Recap ll 
+
+const players = {
+    name: "Nico",
+    age: 98,
+};
+
+players.name = "nicolas"
+console.log(players, console)
+
+// variable 안에 variable은 property라고 부르긴 한다.
+// console은 object였다. ( 오브젝트가 정확히 뭘까? )
+// console 도 log로 조회해보면 console이라는 자바의 객체 모음을 확인할 수 있다. 
+// console도 사실 variable의 모임이다. 또 player.name은 추후에 업데이트할 수 있다. 
+// js 는 위에서 아래로 실행된다. const는 수정 안되지만 안의 내용물을 수정하는 건 상관없다. 
+
+function caption(msg){
+    alert(msg)
+}
+
+caption("hi")
+
+// 2.11 Returns 
+
+// console log는 이제 너무 진부하다!!!! 코드 내에서 답을 구하고 싶어질것이다. 
+// 그러니까 화면 송출이나.. 아니면 변경이나 눈에 보이는 게 나도 하고 싶긴 했다 ㅋ 
+// 예를 들어 뭐 사이트에서 데이터 받아서 작동하는 것 같은거 해보고 싶다! 
+
+const calculator= {
+
+plus1: function(a,b){
+    return alert(a + b);
+},
+minus: function(a,b){
+    return alert(a - b);
+},
+times: function(a,b){
+    return alert(a * b);
+},
+divide: function(a,b){
+    return alert(a / b);
+},
+power: function(a,b){
+    return alert(a ** b);
+},
+};
+
+/*
+calculator.plus1(2, 3);
+calculator.minus(2, 3);
+calculator.times(2, 3);
+calculator.divide(2, 3);
+calculator.power(2, 3);
+
+console.log(calculator.plus(2,3));
+*/
+
+// 이런 식으로 하게 되면 calculator 자체의 값은 존재하지 않는다.
+
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner + 2;    
+}
+
+const krAge = calculateKrAge(age);
+
+console.log(calculateKrAge)
+
+// 이렇게 하게 되면 age에 96을 호출해서 fucntion을 가고 96 +2 가 실행되고 98이 return 된다.
+// 이것도 파이썬이랑 똑같넹 ㅋㅋ 개꿀쥄 ㅋ 
+
+const plusResult = calculator.plus1(2, 3);
+const minusResult = calculator.minus(2, plusResult);
+console.log(plusResult);
+
+// 이렇게 하게 되면 plusResult를 return 값으로 가지고 반환하게 된다.
+// 또 plus, minus가 상호의존적으로 구성될 수가 있다. 
+// 또 이러면 콘솔창에서 각각의 값에 접근할 수가 있다. variable의 value를 지정할 수 있게 된다. 
+// 또 return 앞의 명령어까지만 실행한다.
+
+
+
+
+
+
 
